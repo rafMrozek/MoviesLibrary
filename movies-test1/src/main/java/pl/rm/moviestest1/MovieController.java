6720cc7784d68b3c6406e871532917f919ed8766
Rafal Mrozek <raf.mrozek@gmail.com>
2024-01-18 22:02:43 +0100

    Changed MovieController (ability to add data from external api to H2 database).

--- movies-test1/src/main/java/pl/rm/moviestest1/MovieController.java
+++ movies-test1/src/main/java/pl/rm/moviestest1/MovieController.java
@@ -1,5 +1,6 @@
 package pl.rm.moviestest1;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -10,30 +11,29 @@ import reactor.core.publisher.Mono;
 public class MovieController {
 
     private final OmdbApiService omdbApiService;
+    private final MovieRepository movieRepository;
 
     @Value("${omdb.apiKey}")
     private String omdbApiKey;
 
-    public MovieController(OmdbApiService omdbApiService) {
+    @Autowired
+    public MovieController(OmdbApiService omdbApiService, MovieRepository movieRepository) {
         this.omdbApiService = omdbApiService;
+        this.movieRepository = movieRepository;
     }
 
     @GetMapping("/search")
     public Mono<MovieEntity> searchMovieByTitle(@RequestParam String title) {
-        Mono<MovieEntity> movieEntityMono = omdbApiService.searchByTitle(title, omdbApiKey)
+        return omdbApiService.searchByTitle(title, omdbApiKey)
                 .map(movie -> {
                     MovieEntity movieEntity = new MovieEntity();
                     movieEntity.setMovieTitle(movie.getTitle());
                     movieEntity.setReleaseYear(movie.getYear());
+
+                    movieRepository.save(movieEntity);
+
                     return movieEntity;
                 });
-
-        movieEntityMono.subscribe(movieEntity -> {
-            System.out.println("Title: " + movieEntity.getMovieTitle());
-            System.out.println("Year: " + movieEntity.getReleaseYear());
-        });
-
-        return movieEntityMono;
     }
 }
 
